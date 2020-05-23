ea169d244cc9992f320daae6c147360b29ccf7e3
givememyfiletofish <givememyfiletofish@hotmail.com>
2020-05-23 13:10:39 -0400

    fix BEM naming for input and refactor js code

--- index.js
+++ index.js
@@ -6,35 +6,31 @@ const editButton = document.querySelector(".profile__edit-button");
 const editFormSection = document.querySelector(".edit-form");
 const editForm = editFormSection.querySelector(".edit-form__form");
 const closeEditButton = editFormSection.querySelector(".edit-form__close");
-const nameInput = editFormSection.querySelector(".edit-form__name-input");
-const aboutMeInput = editFormSection.querySelector(
-  ".edit-form__about-me-input"
-);
+const nameInput = editFormSection.querySelector("#name-input");
+const aboutMeInput = editFormSection.querySelector("#about-me-input");
 
-editButton.addEventListener("click", function () {
+editButton.addEventListener("click", showPopup);
+
+function showPopup() {
   editFormSection.classList.add("popup_opened");
 
   nameInput.value = profileName.textContent;
   aboutMeInput.value = profileAboutMe.textContent;
-});
+}
 
 closeEditButton.addEventListener("click", closeEditForm);
-
-editForm.addEventListener("submit", formSubmitHandler);
 
 function closeEditForm() {
   editFormSection.classList.remove("popup_opened");
 }
 
+editForm.addEventListener("submit", formSubmitHandler);
+
 function formSubmitHandler(e) {
   e.preventDefault();
-  let newName = nameInput.value;
-  let newAboutMe = aboutMeInput.value;
 
-  profileName.textContent = newName;
-  profileAboutMe.textContent = newAboutMe;
-  nameInput.value = "";
-  aboutMeInput.value = "";
+  profileName.textContent = nameInput.value;
+  profileAboutMe.textContent = aboutMeInput.value;
 
   closeEditForm();
 }
