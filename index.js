2739a9cdbe70c07b60dd3cff22310134aea7b77b
givememyfiletofish <givememyfiletofish@hotmail.com>
2020-05-24 13:47:26 -0400

    fix BEM

--- index.js
+++ index.js
@@ -6,31 +6,30 @@ const editButton = document.querySelector(".profile__edit-button");
 const editFormSection = document.querySelector(".edit-form");
 const editForm = editFormSection.querySelector(".edit-form__form");
 const closeEditButton = editFormSection.querySelector(".edit-form__close");
-const nameInput = editFormSection.querySelector("#name-input");
-const aboutMeInput = editFormSection.querySelector("#about-me-input");
-
-editButton.addEventListener("click", showPopup);
+const nameInput = editFormSection.querySelector(
+  ".edit-form__input[name='name']"
+);
+const aboutMeInput = editFormSection.querySelector(
+  ".edit-form__input[name='about-me']"
+);
 
 function showPopup() {
-  editFormSection.classList.add("popup_opened");
-
+  editFormSection.classList.add("popup-opened");
   nameInput.value = profileName.textContent;
   aboutMeInput.value = profileAboutMe.textContent;
 }
 
-closeEditButton.addEventListener("click", closeEditForm);
-
-function closeEditForm() {
-  editFormSection.classList.remove("popup_opened");
+function closePopup() {
+  editFormSection.classList.remove("popup-opened");
 }
-
-editForm.addEventListener("submit", formSubmitHandler);
 
 function formSubmitHandler(e) {
   e.preventDefault();
-
   profileName.textContent = nameInput.value;
   profileAboutMe.textContent = aboutMeInput.value;
-
-  closeEditForm();
+  closePopup();
 }
+
+editButton.addEventListener("click", showPopup);
+closeEditButton.addEventListener("click", closePopup);
+editForm.addEventListener("submit", formSubmitHandler);
