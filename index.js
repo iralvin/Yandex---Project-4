e6e729810ea4dc134516dd1748ca252d9d48ef59
givememyfiletofish <givememyfiletofish@hotmail.com>
2020-06-09 11:26:24 -0400

    refactored html/js code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const profileAboutMe = document.querySelector(".profile__about-me");
 const editButton = document.querySelector(".profile__edit-button");
 const editFormSection = document.querySelector(".popup_type_edit");
 const editForm = editFormSection.querySelector(".popup__form");
-const editCloseButton = editFormSection.querySelector(".popup__close_type_edit");
+const editCloseButton = editFormSection.querySelector(".popup__close");
 const saveButton = editForm.querySelector(".save-button");
 const nameInput = editForm.querySelector(".popup__input_type_name");
 const aboutMeInput = editForm.querySelector(".popup__input_type_about-me");
@@ -12,15 +12,15 @@ const aboutMeInput = editForm.querySelector(".popup__input_type_about-me");
 const addButton = document.querySelector(".profile__add-button");
 const addFormSection = document.querySelector(".popup_type_add");
 const addForm = addFormSection.querySelector(".popup__form");
-const addCloseButton = addFormSection.querySelector(".popup__close_type_add");
+const addCloseButton = addFormSection.querySelector(".popup__close");
 const createButton = addForm.querySelector(".create-button");
 const titleInput = addForm.querySelector(".popup__input_type_title");
 const imageLinkInput = addForm.querySelector(".popup__input_type_image-link");
 
 const elementsList = document.querySelector(".elements__list");
 
-const imageCloseButton = document.querySelector(".popup__close_type_image");
 const imagePopup = document.querySelector(".popup_type_image");
+const imageCloseButton = imagePopup.querySelector(".popup__close");
 
 const locationTemplate = document.querySelector("#location-template").content;
 
@@ -54,6 +54,32 @@ const initialCards = [
   },
 ];
 
+// Function to toggle full size image
+function toggleImage() {
+  imagePopup.classList.toggle("popup_opened");
+}
+
+// Function to view larger image when clicked
+function viewFullImage(e) {
+  const fullSizeImage = imagePopup.querySelector(".popup__full-size-image");
+  const imageCaption = imagePopup.querySelector(".popup__image-caption");
+  toggleImage();
+  fullSizeImage.src = this.src;
+  imageCaption.textContent = e.target.nextElementSibling.querySelector(
+    ".elements__location-name"
+  ).textContent;
+}
+
+// Function to toggle liking picture when clicking like button (heart)
+function toggleLikedPicture(e) {
+  e.target.classList.toggle("elements__like_liked");
+}
+
+// Function to delete location cards when clicking trash icon
+function deleteElementCard(e) {
+  e.target.closest(".elements__list-item").remove();
+}
+
 // Function to create new location cards
 function createLocationCard(locationTitle, locationLink) {
   const locationClone = locationTemplate.cloneNode(true);
@@ -74,75 +100,50 @@ function createLocationCard(locationTitle, locationLink) {
   elementsList.prepend(locationClone);
 }
 
-// Function to view larger image when clicked
-function viewFullImage(e) {
-  const imageContainer = imagePopup.querySelector(".popup__image-container");
-  const fullSizeImage = imagePopup.querySelector(".popup__full-size-image");
-  const imageCaption = imagePopup.querySelector(".popup__image-caption");
-
-  imagePopup.classList.toggle("popup_opened");
-
-  fullSizeImage.src = this.src;
-  imageCaption.textContent = e.target.nextElementSibling.querySelector(".elements__location-name").textContent;
+// Function to populate edit form when opened
+function populateProfileEditForm() {
+  nameInput.value = profileName.textContent;
+  aboutMeInput.value = profileAboutMe.textContent;
 }
 
-// Function to toggle liking picture when clicking like button (heart)
-function toggleLikedPicture(e) {
-  e.target.classList.toggle("elements__like_liked");
+// Function to toggle popup edit menu
+function toggleEdit() {
+  editFormSection.classList.toggle("popup_opened");
 }
 
-// Function to delete location cards when clicking trash icon
-function deleteElementCard(e) {
-  e.target.closest(".elements__list-item").remove();
+// Function to toggle popup add location menu
+function toggleAdd() {
+  addFormSection.classList.toggle("popup_opened");
 }
 
-// Function to toggle popups depending if edit menu, add location, or viewing full size image
-function togglePopup(e) {
-  if (e.target === editButton) {
-    editFormSection.classList.toggle("popup_opened");
-    nameInput.value = profileName.textContent;
-    aboutMeInput.value = profileAboutMe.textContent;
-  } else if (e.target === editCloseButton || e.target === saveButton) {
-    editFormSection.classList.toggle("popup_opened");
-  } else if (e.target === addButton || e.target === addCloseButton || e.target === createButton) {
-    addFormSection.classList.toggle("popup_opened");
-  } else if (e.target === imageCloseButton) {
-    imagePopup.classList.toggle("popup_opened");
-  }
-}
-
-// Function to handle form when submitted. Edit form will save new name/about me. 
-// Add form will create new location card based on name and image link.
-function formSubmitHandler(e) {
+// Function to handle edit form when submitted. Edit form will save new name/about me.
+function editFormSubmitHandler(e) {
   e.preventDefault();
-  if (e.target === editForm) {
-    if (nameInput.value.trim() !== "" && aboutMeInput.value.trim() !== "") {
-      profileName.textContent = nameInput.value.trim();
-      profileAboutMe.textContent = aboutMeInput.value.trim();
-    }
-  } else if (e.target === addForm) {
-	if (titleInput.value.trim() !== "" && imageLinkInput.value.trim() !== "") {
-	  createLocationCard(titleInput.value.trim(), imageLinkInput.value.trim());
-
-	  titleInput.value = "";
-	  imageLinkInput.value = "";
-	}
-  }
+  profileName.textContent = nameInput.value.trim();
+  profileAboutMe.textContent = aboutMeInput.value.trim();
+  toggleEdit();
 }
 
-// Map through pre-existing locations to create initial location cards on page load
-initialCards.map(function (location) {
+// Function to handle add location form when submitted. Will create new location card.
+function addFormSubmitHandler(e) {
+  e.preventDefault();
+  createLocationCard(titleInput.value.trim(), imageLinkInput.value.trim());
+  addForm.reset();
+  toggleAdd();
+}
+
+// Iterate through pre-existing locations with forEach to create initial location cards on page load
+initialCards.forEach((location) => {
   createLocationCard(location.name, location.link);
 });
 
-editButton.addEventListener("click", togglePopup);
-editCloseButton.addEventListener("click", togglePopup);
-saveButton.addEventListener("click", togglePopup);
-editForm.addEventListener("submit", formSubmitHandler);
+editButton.addEventListener("click", populateProfileEditForm);
+editButton.addEventListener("click", toggleEdit);
+editCloseButton.addEventListener("click", toggleEdit);
+editForm.addEventListener("submit", editFormSubmitHandler);
 
-addButton.addEventListener("click", togglePopup);
-addCloseButton.addEventListener("click", togglePopup);
-createButton.addEventListener("click", togglePopup);
-addForm.addEventListener("submit", formSubmitHandler);
+addButton.addEventListener("click", toggleAdd);
+addCloseButton.addEventListener("click", toggleAdd);
+addForm.addEventListener("submit", addFormSubmitHandler);
 
-imageCloseButton.addEventListener("click", togglePopup);
+imageCloseButton.addEventListener("click", toggleImage);
